61d8b0012f3a87a798c44f676a58f4d2ad5dc0f4
Bilal Ahmed <bilal.ahmed@o3interfaces.com>
2020-04-07 11:56:37 +0500

    implement Example project

--- Example/Fadeable/ViewController.swift
+++ Example/Fadeable/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Box: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        Box.alpha = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,5 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func toggleAction(_ sender: Any) {
+        if Box.alpha == 1 {
+            UIView.animate(withDuration: 2) {
+                self.Box.alpha = 0
+            }
+        }
+        else {
+            UIView.animate(withDuration: 2) {
+                self.Box.alpha = 1
+            }
+        }
+    }
 }
 
